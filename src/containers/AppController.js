e87ab91dc249bcdcf60e2c034608caec6999ceba
Sezer Berkay Aksu <sezerberkayaksu@gmail.com>
2021-12-15 19:13:53 +0300

    Added local storage support

--- src/containers/AppController.js
+++ src/containers/AppController.js
@@ -20,16 +20,37 @@ export const AppController = () =>{
     if ("geolocation" in navigator) {
       if(location === null){
         navigator.geolocation.getCurrentPosition((position=>{
-          setLocation({lat: position.coords.latitude.toFixed(4), lng: position.coords.longitude.toFixed(4)});
+          setLocation({lat: parseFloat(position.coords.latitude.toFixed(4)), lng: parseFloat(position.coords.longitude.toFixed(4))});
           setTimeout(()=>{
             setLoadingPhase(2);
           },500)
         }));
       }else{
+        let storageChange = false;
         if(loadingPhase === 2){
+          const [oldLat, oldLng] = [parseFloat(localStorage.getItem('lat')), parseFloat(localStorage.getItem('lng'))];
+          if(isNaN(oldLat) || isNaN(oldLng)){
+            storageChange=true
+            localStorage.setItem("lat",location.lat);
+            localStorage.setItem("lng",location.lng);
+          }else{
+            if(Math.abs(oldLat-location.lat) > 0.0001){
+              storageChange = true;
+              localStorage.setItem("lat",location.lat);
+              localStorage.setItem("lng",location.lng);
+            }else if(Math.abs(oldLng-location.lng) > 0.0001){
+              storageChange = true;
+              localStorage.setItem("lat",location.lat);
+              localStorage.setItem("lng",location.lng);
+            }
+          }
+        }
+       if(loadingPhase === 2 && storageChange){
           axios.get(`https://hayalet-sevgilim-backend.herokuapp.com/foundStarbucks?lat=${location.lat}&long=${location.lng}`)
           .then(function (response) {
+            console.log("NEW");
             if(response.status === 200){
+              localStorage.setItem("locationFound",response.data.LocationFound);
               if(response.data.LocationFound){
                 setLoadingPhase(3);
                 setTimeout(()=>{
@@ -48,6 +69,21 @@ export const AppController = () =>{
               setError(true);
             },500)
           })
+        }else if(loadingPhase===2){
+          const oldLocationFound = localStorage.getItem('locationFound');
+          console.log("OLD",localStorage);
+          if(oldLocationFound === "true"){
+            setTimeout(()=>{
+              setLoadingPhase(3);
+              setTimeout(()=>{
+                setLoading(false);
+              },1000)
+            },1000)
+          }else{
+            setTimeout(()=>{
+              setError(true);
+            },1000)
+          }
         }
       }
     } else {
